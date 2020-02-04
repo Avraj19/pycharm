6f0e2a89aa7f6dfabf5264965e6258d2d2259d60
Avraj Singh <ASingh@spartaglobal.com>
2020-02-04 15:17:25 +0000

    strings

---
+++ 102_data_types.py
@@ -0,0 +1,18 @@
+# strings
+# These ara a list of characters put together in a list.
+
+# my_string = "Amazing grillled Fish"
+# print(my_string)
+# print(type(my_string))
+
+# joining of two strings
+# first_name = "Avraj"
+# last_name = "Singh"
+# print(first_name +' '+ last_name)
+
+# Interpolation
+# you inject a string into another string
+# name = "Lana"
+# welcome_message = "Welcome to the DDDDAAAANGERSZZZONE"
+
+# print(f"Welcome {name}, to the DDDAAAANNNNGGGGEEERRZZZONNE!!!!")
